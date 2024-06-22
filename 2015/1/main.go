b0cf4b9e1db0cb92d46dc9997c0b72f6f45ffd3b
Marco Zulian <codebymarco1@gmail.com>
2024-06-22 14:15:16 -0300

    refactors code to get input to helper

--- 2015/1/main.go
+++ 2015/1/main.go
@@ -1,57 +1,20 @@
 package main
 
 import (
+	"adventofcode/internal/helpers"
 	"fmt"
-	"io"
-	"net/http"
-	"net/http/cookiejar"
-	"net/url"
-	"os"
 )
 
+var year = 2015
+var day = 1
+
 func main() {
-	jar, _ := cookiejar.New(nil)
-
-	client := &http.Client{
-		Jar: jar,
-	}
-
-	cookie := &http.Cookie{
-		Name:  "session",
-		Value: "53616c7465645f5fb4fecb996933f2e59abe5cd067a5ff7f7b085a877b1c2eb09f3be29343e0cd71866b239fca250063591ab5c9c334d84e7cd6d3442be1ca13",
-	}
-
-	requestURL := "https://adventofcode.com/2015/day/1/input"
-	parsedURL, err := url.Parse(requestURL)
-	if err != nil {
-		fmt.Println("Error parsing request:", err)
-		return
-	}
-
-	jar.SetCookies(parsedURL, []*http.Cookie{cookie})
-	req, err := http.NewRequest("GET", requestURL, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		os.Exit(1)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		os.Exit(1)
-	}
-
-	b, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		os.Exit(1)
-	}
-
 	var floor int
 	var firstBasement int
 
-	for i, rn := range b {
+	input, _ := helpers.GetInput(year, day)
+
+	for i, rn := range input {
 		if rn == '(' {
 			floor++
 		} else {
---
+++ internal/helpers/helpers.go
@@ -0,0 +1,55 @@
+package helpers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+)
+
+func GetInput(day, year int) ([]byte, error) {
+	file, err := os.ReadFile("input.txt")
+	if err == nil {
+		return file, nil
+	}
+
+	jar, _ := cookiejar.New(nil)
+
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	cookie := &http.Cookie{
+		Name:  "session",
+		Value: "53616c7465645f5fb4fecb996933f2e59abe5cd067a5ff7f7b085a877b1c2eb09f3be29343e0cd71866b239fca250063591ab5c9c334d84e7cd6d3442be1ca13",
+	}
+
+	requestURL := "https://adventofcode.com/2015/day/1/input"
+	parsedURL, err := url.Parse(requestURL)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing URL %s", requestURL)
+	}
+
+	jar.SetCookies(parsedURL, []*http.Cookie{cookie})
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request for URL %s", requestURL)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request to URL %s", requestURL)
+	}
+
+	content, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error reading response %s", err)
+	}
+
+	err = os.WriteFile("input.txt", content, 0777)
+	fmt.Println(err)
+	return content, nil
+}
