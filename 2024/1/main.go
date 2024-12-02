fef1fb6ed206a8af488e26aa038da2b7c2d487b0
Marco Zulian <codebymarco1@gmail.com>
2024-12-02 20:54:53 -0300

    solves 2024 day 1 part 2

--- 2024/1/main.go
+++ 2024/1/main.go
@@ -16,8 +16,9 @@ func main() {
 	input, _ := helpers.GetInput(year, day)
 
 	distance := PartOneSolution(input)
+	similarityScore := PartTwoSolution(input)
 
-	fmt.Printf("Total distance: %d.\n", distance)
+	fmt.Printf("Total distance: %d. Similarity: %d\n", distance, similarityScore)
 }
 
 func parseInput(input []byte) ([]int, []int) {
@@ -46,4 +47,32 @@ func PartOneSolution(input []byte) int {
 	}
 
 	return distance
+}
+
+func PartTwoSolution(input []byte) int {
+	var similarityScore int
+	first, second := parseInput(input)
+	i, j := 0, 0
+	curr := 0
+
+	for i < len(first) && j < len(second) {
+		curr = first[i]
+		countFirst, countSecond := 0, 0
+
+		for i < len(first) && first[i] == curr {
+			countFirst += 1
+			i += 1
+		}
+
+		for j < len(second) && second[j] <= curr {
+			if second[j] == curr {
+				countSecond += 1
+			}
+			j += 1
+		}
+
+		similarityScore += countFirst * countSecond * curr
+	}
+
+	return similarityScore
 }
